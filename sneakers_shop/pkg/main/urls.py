54daaf216d2cfe6e31275f5de76e49b53d8fcf88
Oleksandr Rehush <o.rehush@bvblogic.com>
2016-03-11 22:21:10 +0100

    add title to context, change base.html

--- sneakers_shop/pkg/main/urls.py
+++ sneakers_shop/pkg/main/urls.py
@@ -3,7 +3,7 @@ from sneakers_shop.pkg.main.views import IndexView, AboutView, ContactsView, \
     CatalogView
 
 urlpatterns = [
-    url(r'^', IndexView.as_view()),
+    url(r'^$', IndexView.as_view()),
     url(r'^about/', AboutView.as_view()),
     url(r'^contacts/', ContactsView.as_view()),
     url(r'^catalog/', CatalogView.as_view()),
--- sneakers_shop/pkg/main/views.py
+++ sneakers_shop/pkg/main/views.py
@@ -1,17 +1,38 @@
 from django.views import generic
+from django.utils.translation import ugettext_lazy as _
 
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = _("Головна")
+        return context
+
 
 class ContactsView(generic.TemplateView):
     template_name = 'contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactsView, self).get_context_data(**kwargs)
+        context['title'] = _("Контактна інформація")
+        return context
 
 
 class AboutView(generic.TemplateView):
     template_name = 'about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['title'] = _("Про нас")
+        return context
+
 
 class CatalogView(generic.TemplateView):
     template_name = 'catalog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CatalogView, self).get_context_data(**kwargs)
+        context['title'] = _("Каталог. Вибери свій товар.")
+        return context

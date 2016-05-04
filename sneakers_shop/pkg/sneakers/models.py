2a352119f9e385a5308a812174ed35df060b03c3
Oleksandr Rehush <o.rehush@bvblogic.com>
2016-05-04 18:48:45 +0300

    hotfix

--- sneakers_shop/pkg/sneakers/models.py
+++ sneakers_shop/pkg/sneakers/models.py
@@ -161,7 +161,7 @@ class SneakersPhoto(models.Model):
         self.thumbs.save(suf.name+'.jpg', suf, save=False)
 
     def save(self, **kwargs):
-        if not self.image:
+        if not self.image or self.DEFAULT_IMG not in self.thumbs:
             return super(SneakersPhoto, self).save(**kwargs)
         self.create_thumbs()
         return super(SneakersPhoto, self).save(**kwargs)
